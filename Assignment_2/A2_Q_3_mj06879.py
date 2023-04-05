72a74e68be2988ff015fcb431286863138e9f256
mj06879 <mj06879@st.habib.edu.pk>
2023-04-05 10:55:44 +0500

    Q_3_updated

--- Assignment_2/A2_Q_3_mj06879.py
+++ Assignment_2/A2_Q_3_mj06879.py
@@ -9,9 +9,7 @@ LEFT_COLUMN = list(range(0, WIDTH * HEIGHT, WIDTH))
 RIGHT_COLUMN = list(range(WIDTH - 1, WIDTH * HEIGHT, WIDTH))
 ACTIONS = [-1, 1, -1 * HEIGHT, HEIGHT] # left, right, up, down
 action_dict = {-1: 'left', 1: 'right', -1 * HEIGHT: 'up', HEIGHT: 'down'}
-grid = np.zeros(HEIGHT * WIDTH)
-gridCounts = np.zeros(HEIGHT * WIDTH)
-avgCounts = np.zeros(HEIGHT * WIDTH)
+DISCOUNT = 0.9
 
 def nextState(state, action):
     if state == 1:
@@ -44,7 +42,7 @@ def runEpisode(state):
     while True:
         action = random.choice(ACTIONS)
         next_state, reward = nextState(state, action)
-#         print(1, next_state, reward)
+
         if next_state == -1:
             return episode, episodeReward
         else:
@@ -54,6 +52,38 @@ def runEpisode(state):
     return episode, episodeReward
 
 
-state = startingState()
-e, eR = runEpisode(state)
-print(e, eR, sep = "\n\n")
+def Optimal_Value_function():      # upgraded figure_3_2()
+    it = 0
+    grid = np.zeros(HEIGHT * WIDTH)         # V(s)
+    gridCounts = np.zeros(HEIGHT * WIDTH)   # N(s)
+    avgCounts = np.zeros(HEIGHT * WIDTH)    # Returns(s)
+
+    while True:
+        # keep iteration until convergence
+        new_value = np.copy(grid)      # V'(s)
+        state = startingState()
+        e, eR = runEpisode(state)
+        Ns_check = np.zeros(HEIGHT * WIDTH)   # N(s)_for temp_of_first_visit_Record
+        avr = 0
+        for i in range(len(e)):           # For Each episode
+            if Ns_check[e[i]] == 0:       # For first-visit MC
+                Ns_check[e[i]] = 1        # updated only once in each episode
+                gridCounts[e[i]] += 1     # main update in N(s)
+                for j in range(len(e)-i):
+                    avr +=  (DISCOUNT**j)*eR[j+i]
+                avgCounts[e[i]] = avr
+                new_value[e[i]] = grid[e[i]] + (1/(gridCounts[e[i]] + 1))*(avgCounts[e[i]] - grid[e[i]])
+                                
+                # V_new(s) = V_old(s) + (\frac{1}{N(s)+1})*(Returns(s) - V_old(s))
+        if np.sum(np.abs(grid - new_value)) < 1e-2:
+            break
+        grid = new_value
+        it += 1
+        # input("Press Enter to continue...")
+        np.set_printoptions(precision=2)
+        print(grid)
+        print()
+    print("Converges in {} iterations".format(it))
+    return grid
+        
+Optimal_Value_function()
